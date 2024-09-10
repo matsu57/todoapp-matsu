d3242883e16adf25f44bb5d8b2c1d9f1c2c1b77e
matsu_web <rm.57.boc@gmail.com>
2024-09-10 15:20:28 +0900

    ダミーデータを作成し表示させる

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Board.create
+john = User.create!(email: 'john@sample.com', password: 'password')
+emily = User.create!(email: 'emily@sample.com', password: 'password')
+# .create!で絶対に保存される（何かあったら絶対エラーが出る）
+
+3.times do
+  john.boards.create!(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
+
+# 5.times do
+# # 5回繰り返す
+#   emily.articles.create!(
+#     title: Faker::Lorem.sentence(word_count: 5),
+#     content: Faker::Lorem.sentence(word_count: 100)
+#   )
+# end
