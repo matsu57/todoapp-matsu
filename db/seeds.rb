18f3ec1e766fc58ad889153fd206599cdae9133d
matsu_web <rm.57.boc@gmail.com>
2024-09-15 15:58:07 +0900

    ダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-john = User.create!(email: 'john@sample.com', password: 'password')
-emily = User.create!(email: 'emily@sample.com', password: 'password')
+mark = User.create!(email: 'mark@sample.com', password: 'password')
+ema = User.create!(email: 'ema@sample.com', password: 'password')
 # .create!で絶対に保存される（何かあったら絶対エラーが出る）
 
-3.times do
-  john.boards.create!(
+2.times do
+  mark.boards.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
 end
 
-# 5.times do
-# # 5回繰り返す
-#   emily.articles.create!(
+# 2.times do
+#   ema.boards.create!(
 #     title: Faker::Lorem.sentence(word_count: 5),
 #     content: Faker::Lorem.sentence(word_count: 100)
 #   )
