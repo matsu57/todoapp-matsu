4d149d891ad433d9cb6ec84a9ac759d8a2cea115
matsu_web <rm.57.boc@gmail.com>
2024-09-16 09:38:58 +0900

    task関係のmigrateファイル修正

--- db/migrate/20240915122451_create_add_board_id_to_tasks.rb
+++ db/migrate/20240915122451_create_add_board_id_to_tasks.rb
@@ -1,8 +1,5 @@
 class CreateAddBoardIdToTasks < ActiveRecord::Migration[6.0]
   def change
-    create_table :add_board_id_to_tasks do |t|
-      add_reference :tasks, :board
-      t.timestamps
-    end
+    add_reference :tasks, :board
   end
 end
