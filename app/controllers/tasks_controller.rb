ae52dcc08c680594f869e75496237dbfee3dd969
matsu_web <rm.57.boc@gmail.com>
2024-09-18 16:18:56 +0900

    taskのdeleteアクションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_board
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -20,23 +22,34 @@ class TasksController < ApplicationController
   end
 
   def edit
-    board = Board.find(params[:board_id])
-    @task = board.tasks.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:board_id])
-    @task = board.tasks.find(params[:id])
     if @task.update(task_params)
-      redirect_to board_task_path(@task), notice: 'タスクを更新しました'
+      redirect_to board_task_path(@board, @task), notice: 'タスクを更新しました'
     else
       flash.now[:error] = 'タスクの更新に失敗しました'
       render :edit
     end
   end
 
+  def destroy
+    @task.destroy!
+    redirect_to board_path(@board), notice: '削除に成功しました'
+  end
+
   private
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
+  def set_task
+    @task = @board.tasks.find(params[:id])
+  end
+  
   def task_params
     params.require(:task).permit(:title, :content).merge(user_id: current_user.id)
   end
+
+
 end
