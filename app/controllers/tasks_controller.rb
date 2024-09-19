ed4674e477e6cd5882bc55473a528c37bd1d6d96
matsu_web <rm.57.boc@gmail.com>
2024-09-19 16:28:28 +0900

    ログインしている場合のみ:new, :create, :edit, :update, :destroyを許可する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_board
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def show
     @comments = @task.comments
