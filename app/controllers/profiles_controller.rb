57789019ef8397b71d8d873b8df22901621a7c18
matsu_web <rm.57.boc@gmail.com>
2024-09-19 21:24:52 +0900

    profileのupdateアクションを追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,19 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新しました'
+    else
+      flash.now[:error] = 'プロフィールの更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:name)
+  end
 end
