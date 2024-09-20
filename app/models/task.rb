38abc293d910993d6bfa2995df9bdb373cc863c8
matsu_web <rm.57.boc@gmail.com>
2024-09-20 09:50:04 +0900

    eyecatchを保存できるようにする

--- app/models/task.rb
+++ app/models/task.rb
@@ -20,6 +20,7 @@ class Task < ApplicationRecord
   belongs_to :board
 
   has_many :comments, dependent: :destroy
+  has_one_attached :eyecatch
 
   validates :title, presence: true
   validates :content, presence: true
