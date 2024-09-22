ce79bf89b23da48e0d4689c6c996a5626dcffaea
matsu_web <rm.57.boc@gmail.com>
2024-09-22 17:13:08 +0900

    taskがないときエラーが発生したので修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @tasks = @board.tasks.all
-    @commenters = create_commenters_list(@tasks)
+    
   end
 
   def show
@@ -19,7 +18,7 @@ class TasksController < ApplicationController
   def create
     @task = @board.tasks.build(task_params)
     if @task.save
-      redirect_to board_task_path(@board, @task), notice: 'タスクを追加しました'
+      redirect_to board_path(@board), notice: 'タスクを追加しました'
     else
       flash.now[:error] = 'タスクの保存に失敗しました'
       render :new
@@ -55,12 +54,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :content, :eyecatch, :deadline).merge(user_id: current_user.id)
   end
-
-  def create_commenters_list(tasks)
-    tasks.each_with_object({}) do |task, hash|
-      hash[task.id] = task.comments.map(&:user).uniq - [task.user]
-    end
-  end
-
-
 end
