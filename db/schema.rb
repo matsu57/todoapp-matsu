36bdb84a1a1413a14a0caf0cf982adbceb5d2889
matsu_web <rm.57.boc@gmail.com>
2024-09-22 21:59:23 +0900

    データベースのリセット、再migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_21_005658) do
+ActiveRecord::Schema.define(version: 2024_09_19_073638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,13 +64,13 @@ ActiveRecord::Schema.define(version: 2024_09_21_005658) do
   end
 
   create_table "tasks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
     t.string "title"
     t.text "content"
+    t.date "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.bigint "board_id"
-    t.date "deadline"
     t.index ["board_id"], name: "index_tasks_on_board_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
