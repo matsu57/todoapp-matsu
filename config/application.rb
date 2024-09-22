de286438e6da02afb48ee54f83fc921262623192
matsu_web <rm.57.boc@gmail.com>
2024-09-22 16:28:50 +0900

    本番環境ではdotenvを読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Todoapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # .envを読み込む
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
